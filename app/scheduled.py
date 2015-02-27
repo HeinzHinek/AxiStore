e0bbaf5d3d93e5d212a6196c77634404f7c59971
Hinek <heinzzz@gmail.com>
2015-02-27 09:43:55 +0900

    'MySQL server has gone' issue fix - thread safe sessioning

--- app/scheduled.py
+++ app/scheduled.py
@@ -4,6 +4,8 @@ from app import db
 from models import Product
 from config import TEMP_FILES_PATH, LASTURA_SKLAD_URL
 from csvHelper import parse_csv, generate_axismart_availability_csv
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
 import os, urllib2
 
 def sheduleLastura():
@@ -21,6 +23,13 @@ def sheduleLastura():
     webFile.close()
     localFile.close()
     data = parse_csv(filename)[1:]
+
+    # Thread safe sessioning
+    session_factory = sessionmaker(bind=db.engine)
+    Session = scoped_session(session_factory)
+    local_session = Session()
+    local_session._model_changes = {}   # flask-alchemy & pure sqlalchemy subclassing issue fix
+
     counter = 0
     for item in data:
         maker_code = item[0]
@@ -28,12 +37,15 @@ def sheduleLastura():
             maker_qty_stock = int(item[1])
         except ValueError:
             continue
-        product = Product.query.filter_by(maker_code=maker_code).first()
+        product = local_session.query(Product).filter_by(maker_code=maker_code).first()
         if product:
             product.maker_qty_stock = maker_qty_stock
-            db.session.add(product)
-            db.session.commit()
+            local_session.add(product)
             counter += 1
+
+    local_session.commit()
+    local_session.close()
+    Session.remove()
 
     print "Scheduled job finished sucessfully. Number of updated values for Lastura: " + str(counter)
     return True
